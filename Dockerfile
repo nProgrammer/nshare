3a593acf5e7cad568be5687e8f7048441e8c0111
nProgrammer <ndata.wagner.norbert@gmail.com>
2023-01-08 22:19:17 +0100

    added ssl/tls support

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,20 @@ FROM golang:1.19.2
 
 WORKDIR /api
 COPY go.mod .
-RUN go mod tidy
+COPY go.sum .
 RUN go mod download
 
 COPY main.go .
+ADD certificates certificates
+ADD errors errors
+ADD repositories repositories
 
 RUN go build -o /nshare-api
 
 WORKDIR /
 RUN rm -r /api
+
+ENV WORKINGPORT=8080
+ENV SSLMODE=false
 
 CMD [ "/nshare-api" ]
--- main.go
+++ main.go
@@ -3,16 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	sslmode, err := strconv.ParseBool(os.Getenv("SSLMODE"))
+
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	if sslmode {
+		log.Fatal(http.ListenAndServeTLS(":8080", "certificates/origin.pem", "certificates/private.key", r))
+	} else {
+		log.Fatal(http.ListenAndServe(":"+os.Getenv("WORKINGPORT"), r))
+	}
 }
